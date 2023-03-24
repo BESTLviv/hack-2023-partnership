d026cd08f5070102e6fbaeb69aa18b0da69c533c
anthonybdev <anton.bitner.kn.2021@lpnu.ua>
2023-03-24 15:38:19 +0200

    added scss modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.scss'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <p className={styles.test}>HACKATHON</p>
       </header>
     </div>
   );
