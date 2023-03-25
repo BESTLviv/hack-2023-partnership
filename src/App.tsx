bf7e79a92662bf5386b12bd348c36121cc5c6476
gutovskii <volodimir.gutovskiy@gmail.com>
2023-03-25 03:36:57 +0200

    Hero

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.scss';
 import Partnership from './containers/Partnership';
 
 import { ChakraProvider } from '@chakra-ui/react'
+import { Hero } from './components/hero/Hero';
 
 function App() {
   return (
     <ChakraProvider>
-      <div className='App'>
-        <Partnership />
+      <div className='App' style={{background: '#0D0D0D'}}>
+        <Hero />
       </div>
     </ChakraProvider>
   );
