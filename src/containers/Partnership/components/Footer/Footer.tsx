8344fa7b38aebac472b882dd27a4a6d6002aa1ae
marrrkiv <mariia.markiv.knm.2020@lpnu.ua>
2023-03-26 04:45:55 +0300

    interim commit

--- src/containers/Partnership/components/Footer/Footer.tsx
+++ src/containers/Partnership/components/Footer/Footer.tsx
@@ -1,20 +1,25 @@
 import styles from '../Footer/Footer.module.scss';
 import BestLogo from '../../../../assets/images/logo/logo_best_footer.svg';
-import { Facebook,
-    In,
-    Inst,
-    Telegram, } from '../../../../assets/index';
+import { Facebook, In, Inst, Telegram } from '../../../../assets/index';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <p className={styles.year}>2023</p>
-      <img src={BestLogo} alt="best logo" className='bestLogo'/>
-      <div>
-         <img src={Facebook} />
-         <img src={Inst} />
-         <img src={In} />
-         <img src={Telegram} />
+      <img src={BestLogo} alt="best logo" className={styles.bestLogo} />
+      <div className={styles.socials}>
+        <a href="#" rel="noopener noreferrer" target="_blank">
+          <img src={Facebook} alt="facebook" />
+        </a>
+        <a href="#" rel="noopener noreferrer" target="_blank">
+          <img src={Inst} alt="instagram" />
+        </a>
+        <a href="#" rel="noopener noreferrer" target="_blank">
+          <img src={In} alt="linked" />
+        </a>
+        <a href="#" rel="noopener noreferrer" target="_blank">
+          <img src={Telegram} alt="telegram" />
+        </a>
       </div>
     </div>
   );
