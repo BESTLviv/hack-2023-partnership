37d192aed9178a1949cc58b19a7b541698a99cdf
gutovskii <volodimir.gutovskiy@gmail.com>
2023-03-26 04:49:31 +0300

    Project Organization

---
+++ src/components/ProjectOrganizator/ProjectOrganizator.tsx
@@ -0,0 +1,38 @@
+import { Box, Flex } from '@chakra-ui/react';
+import HeaderText from '../HeaderText';
+
+import BestLogo from '../../assets/images/organiser/best_logo.svg';
+import EuropeMap from '../../assets/images/organiser/europe_map.svg';
+
+import style from '../ProjectOrganizator/ProjectOrganizator.module.scss';
+import BestData, { BestDataProps } from './BestData';
+
+const ProjectOrganizator: React.FC = () => {
+  const bestData: BestDataProps[] = [
+    {count: 86, subject: 'Осередків'},
+    {count: 30, subject: 'Країн'},
+    {count: 34, subject: 'Роки досвіду'}
+  ]; 
+
+  return (
+    <>
+      <HeaderText text="Організатор проєкту"/>
+      <Flex flexDirection={'column'} alignItems={'center'} marginTop="75px">
+        <Flex flexDirection={'row'} justifyContent={'center'} alignItems={'center'} marginBottom="75px" width="620px">
+          <img src={BestLogo} alt="BEST Logo" style={{background: 'transparent', display: 'block'}} width={250}/>
+          <Box className={style.projorg_best_description}>
+            <span className={style.projorg_BEST}>BEST</span> — це неприбуткова громадська організація, яка об’єднує студентів технічних спеціальностей для забезпечення обміну знаннями та співпраці компаній, університетів та студентів Європи.
+          </Box>
+        </Flex>
+        <Flex flexDirection={'row'} position="relative" width={'800px'}>
+          <img src={EuropeMap} alt="Europe Map" width={550}/>
+          <Flex flexDirection={'row'} position={'absolute'} top={'50px'} left="50%">
+            {bestData.map((bd, i) => <BestData key={i} count={bd.count} subject={bd.subject}/>)}  
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
+
+export default ProjectOrganizator;
--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -3,6 +3,7 @@ import { Flex } from '@chakra-ui/react';
 import AnnaHukSvg from '../../assets/images/team/anna_huk.svg';
 import BohdanVivcharSvg from '../../assets/images/team/bohdan_vivchar.svg';
 import SofiaLevSvg from '../../assets/images/team/sofia_lev.svg';
+import HeaderText from '../HeaderText';
 
 import style from '../team/Team.module.scss';
 import Teammate from './Teammate';
@@ -10,7 +11,7 @@ import Teammate from './Teammate';
 const Team: React.FC = () => {
   return (
     <>
-      <h2 className={style.team_header}>Контакти</h2>
+      <HeaderText text="Контакти"/>
       <a className={style.team_anna} href="static/media/anna_huk.51d491cd47c7801a65fecd61b0fa6cba.svg"></a>
       <Flex flexDirection={'row'} justifyContent={'center'} flexWrap={'wrap'}>
         <Teammate 
@@ -23,7 +24,7 @@ const Team: React.FC = () => {
         <Teammate 
           avatarSrc={SofiaLevSvg}
           name="Софія Лев"
-          position={`\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b Головна \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b організаторка`}
+          position={'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b Головна \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b організаторка'}
           number="+380508895899"
           email="levsophia11@gmail.com"
         />
