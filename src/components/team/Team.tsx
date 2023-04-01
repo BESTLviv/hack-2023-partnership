03855369f32ef324e6039b92b7bcee91be937274
nestor <nestornestorr10@gmail.com>
2023-04-01 15:42:24 +0300

    fixed footer and contacts

--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import {Flex} from '@chakra-ui/react';
 
 import AnnaHukSvg from '../../assets/images/team/anna_huk.svg';
 import BohdanVivcharSvg from '../../assets/images/team/bohdan_vivchar.svg';
@@ -9,35 +9,36 @@ import style from '../team/Team.module.scss';
 import Teammate from './Teammate';
 
 const Team: React.FC = () => {
-  return (
-    <>
-      <HeaderText text="Контакти"/>
-      <a className={style.team_anna} href="static/media/anna_huk.51d491cd47c7801a65fecd61b0fa6cba.svg"></a>
-      <Flex flexDirection={'row'} justifyContent={'center'} flexWrap={'wrap'}>
-        <Teammate 
-          avatarSrc={AnnaHukSvg}
-          name="Анна Гук"
-          position="Відповідальна за корпоративні зв'язки"
-          number="+380987567648"
-          email="anna.huk.best@gmail.com"
-        />
-        <Teammate 
-          avatarSrc={SofiaLevSvg}
-          name="Софія Лев"
-          position={'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b Головна \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b організаторка'}
-          number="+380508895899"
-          email="levsophia11@gmail.com"
-        />
-        <Teammate 
-          avatarSrc={BohdanVivcharSvg}
-          name="Богдан Вівчар"
-          position="Відповідальний за корпоративні зв'язки"
-          number="+380987567648"
-          email="bohdanvivchar.best@gmail.com"
-        />
-      </Flex>
-    </>
-  );
+    return (
+        <div>
+            <HeaderText text="Контакти"/>
+            <a className={style.team_anna} href="static/media/anna_huk.51d491cd47c7801a65fecd61b0fa6cba.svg"></a>
+            <Flex flexDirection={'row'} gap={'100px'} alignItems={'stretch'} justifyContent={'center'}
+                  flexWrap={'wrap'}>
+                <Teammate
+                    avatarSrc={AnnaHukSvg}
+                    name="Анна Гук"
+                    position="Відповідальна за корпоративні зв'язки"
+                    number="+380987567648"
+                    email="anna.huk.best@gmail.com"
+                />
+                <Teammate
+                    avatarSrc={SofiaLevSvg}
+                    name="Софія Лев"
+                    position={'\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b Головна \b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b організаторка'}
+                    number="+380508895899"
+                    email="levsophia11@gmail.com"
+                />
+                <Teammate
+                    avatarSrc={BohdanVivcharSvg}
+                    name="Богдан Вівчар"
+                    position="Відповідальний за корпоративні зв'язки"
+                    number="+380987567648"
+                    email="bohdanvivchar.best@gmail.com"
+                />
+            </Flex>
+        </div>
+    );
 }
 
 export default Team;
--- src/components/team/Teammate.tsx
+++ src/components/team/Teammate.tsx
@@ -1,32 +1,35 @@
-import { Flex } from '@chakra-ui/react';
+import {Flex} from '@chakra-ui/react';
 
 import style from '../team/Teammate.module.scss';
 
 interface TeammateProps {
-  avatarSrc: string,
-  name: string;
-  position: string;
-  number: string;
-  email: string
+    avatarSrc: string,
+    name: string;
+    position: string;
+    number: string;
+    email: string
 }
 
 const Teammate: React.FC<TeammateProps> = ({
-  // eslint-disable-next-line react/prop-types
-  avatarSrc, name, position, number, email
-}) => {
-  return (
-    <Flex className={style.teammate} flexDirection={'column'} alignItems={'center'}>
-      <img src={avatarSrc} alt="teammate" className={style.teammate_avatar} width={275}/>
-      <Flex flexDirection={'column'}>
-        <span className={style.teammate_name}>{name}</span>
-        <span className={style.teammate_position}>{position}</span>
-        <Flex className={style.teammate_contacts} flexDirection={'column'}>
-          <span className={style.teammate_number}>{number}</span>
-          <span className={style.teammate_email}>{email}</span>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+                                               // eslint-disable-next-line react/prop-types
+                                               avatarSrc, name, position, number, email
+                                           }) => {
+    return (
+        <div className={style.teammate}>
+            <img src={avatarSrc} alt="teammate" className={style.teammate_avatar} width={275}/>
+            <div className={style.contactBody}>
+                <div>
+                    <p className={style.teammate_name}>{name}</p>
+                    <p className={style.teammate_position}>{position}</p>
+                </div>
+
+                <div className={style.teammate_contacts}>
+                    <p className={style.teammate_number}>{number}</p>
+                    <p className={style.teammate_email}>{email}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Teammate;
