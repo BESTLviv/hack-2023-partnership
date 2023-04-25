62030c737584859085e19fce51ecc4a7a3427d9d
Bohdan Vivchar <bohdan@office.cquelle.de>
2023-04-26 01:35:57 +0300

    Change structure of jsons, and add detect lang by url. Example : "?lng=en"

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,28 +1,39 @@
 // i18n.js
 
 import i18n from 'i18next';
-import {initReactI18next} from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import XHR from 'i18next-http-backend'; // <---- add this
 
 import translationEN from './locales/en.json';
 import translationUK from './locales/uk.json';
 
-export const resources = {
-    en: {
-        translation: translationEN,
-    },
-    uk: {
-        translation: translationUK,
-    },
+const options = {
+  order: ['querystring', 'navigator'],
+  lookupQuerystring: 'lng',
 };
 
-i18n.use(initReactI18next).init({
-    resources,
-    lng: 'en',
-    fallbackLng: 'en',
+export const resources = {
+  en: {
+    translation: translationEN,
+  },
+  uk: {
+    translation: translationUK,
+  },
+};
 
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    detection: options,
+    supportedLngs: ['en', 'uk'],
     interpolation: {
-        escapeValue: false,
+      escapeValue: false,
     },
-});
+  });
 
 export default i18n;
